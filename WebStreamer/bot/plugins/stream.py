3c5206d8c56955b2fb4c494d679189935e3edbd2
alphadlplus <82231865+alphadlplus@users.noreply.github.com>
2021-08-05 18:31:11 +0430

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -1,95 +1,115 @@
-
-# (c) @Avishkarpatil 
-
 import asyncio
+from datetime import datetime, timedelta
 from WebStreamer.bot import StreamBot
 from WebStreamer.utils.database import Database
-from WebStreamer.utils.human_readable import humanbytes
+from WebStreamer.utils.human_readable import humanbytes, size_gb
 from WebStreamer.vars import Var
 from pyrogram import filters, Client
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-db = Database(Var.DATABASE_URL, Var.SESSION_NAME)
+db = Database()
 
 
 @StreamBot.on_message(filters.private & (filters.document | filters.video | filters.audio) & ~filters.edited, group=4)
 async def private_receive_handler(c: Client, m: Message):
-    if not await db.is_user_exist(m.from_user.id):
-        await db.add_user(m.from_user.id)
+    if await db.check_user(m.from_user.id) == False :
         await c.send_message(
-            Var.BIN_CHANNEL,
-            f"Nᴇᴡ Usᴇʀ Jᴏɪɴᴇᴅ : \n\nNᴀᴍᴇ : [{m.from_user.first_name}](tg://user?id={m.from_user.id}) Sᴛᴀʀᴛᴇᴅ Yᴏᴜʀ Bᴏᴛ !!"
+                        chat_id=m.chat.id,
+                        text='''سلام\n
+شما اشتراک تدارید برای خرید اشتراک به ایدی زیر پیام دهید.\n
+<a href='https://t.me/alphadlsup'> AlphaDL Suport</a>''',
+                        parse_mode="HTML",
+                        disable_web_page_preview=True
         )
-    if Var.UPDATES_CHANNEL != "None":
-        try:
-            user = await c.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
-            if user.status == "kicked":
+    if await db.check_user(m.from_user.id) == True:
+        if Var.UPDATES_CHANNEL != "None":
+            try:
+                user = await c.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+                if user.status == "kicked":
+                    await c.send_message(
+                        chat_id=m.chat.id,
+                        text="__شما بن شدید به ادمین پیام دهید__\n\n @alphadlsup",
+                        parse_mode="Markdown",
+                        disable_web_page_preview=True
+                    )
+                    return
+            except UserNotParticipant:
                 await c.send_message(
                     chat_id=m.chat.id,
-                    text="__Sᴏʀʀʏ Sɪʀ, Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴍᴇ.__\n\n  **Cᴏɴᴛᴀᴄᴛ Dᴇᴠᴇʟᴏᴘᴇʀ @Avishkarpatil Tʜᴇʏ Wɪʟʟ Hᴇʟᴘ Yᴏᴜ**",
-                    parse_mode="markdown",
-                    disable_web_page_preview=True
+                    text="""<i>عضو کانال الفا دی ال شوید 🔐!</i>""",
+                    reply_markup=InlineKeyboardMarkup(
+                        [[ InlineKeyboardButton("🤖 عضویت در کانال", url=f"https://t.me/{Var.UPDATES_CHANNEL}") ]]
+                    ),
+                    parse_mode="HTML"
                 )
                 return
-        except UserNotParticipant:
-            await c.send_message(
-                chat_id=m.chat.id,
-                text="""<i>Jᴏɪɴ ᴍʏ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜꜱᴇ ᴍᴇ 🔐</i>""",
-                reply_markup=InlineKeyboardMarkup(
-                    [[ InlineKeyboardButton("Jᴏɪɴ ɴᴏᴡ 🔓", url=f"https://t.me/{Var.UPDATES_CHANNEL}") ]]
-                ),
-                parse_mode="HTML"
-            )
-            return
-        except Exception:
-            await c.send_message(
-                chat_id=m.chat.id,
-                text="**Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍʏ ʙᴏss** @Avishkarpatil",
-                parse_mode="markdown",
-                disable_web_page_preview=True)
-            return
-    try:
-        log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-        stream_link = "https://{}/{}".format(Var.FQDN, log_msg.message_id) if Var.ON_HEROKU or Var.NO_PORT else \
-            "http://{}:{}/{}".format(Var.FQDN,
-                                    Var.PORT,
-                                    log_msg.message_id)
-        file_size = None
-        if m.video:
-            file_size = f"{humanbytes(m.video.file_size)}"
-        elif m.document:
-            file_size = f"{humanbytes(m.document.file_size)}"
-        elif m.audio:
-            file_size = f"{humanbytes(m.audio.file_size)}"
+            except Exception:
+                await c.send_message(
+                    chat_id=m.chat.id,
+                    text="**مشکلی پیش آمده به ادمین اطلاع دهید** [Alphadl suport](https://t.me/alphadlsup).",
+                    parse_mode="Markdown",
+                    disable_web_page_preview=True)
+                return
+        try:
+            log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
+            stream_link = "https://{}/{}".format(Var.FQDN, log_msg.message_id) if Var.ON_HEROKU or Var.NO_PORT else \
+                "http://{}:{}/{}".format(Var.FQDN,
+                                        Var.PORT,
+                                        log_msg.message_id)
+            file_size = None
+            if m.video:
+                file_size = f"{humanbytes(m.video.file_size)}"
+            elif m.document:
+                file_size = f"{humanbytes(m.document.file_size)}"
+            elif m.audio:
+                file_size = f"{humanbytes(m.audio.file_size)}"
 
-        file_name = None
-        if m.video:
-            file_name = f"{m.video.file_name}"
-        elif m.document:
-            file_name = f"{m.document.file_name}"
-        elif m.audio:
-            file_name = f"{m.audio.file_name}"
+            file_name = None
+            if m.video:
+                file_name = f"{m.video.file_name}"
+            elif m.document:
+                file_name = f"{m.document.file_name}"
+            elif m.audio:
+                file_name = f"{m.audio.file_name}"
 
-        msg_text ="""
-<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n
-<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n
-<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n
-<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n
-<b>🚸 Nᴏᴛᴇ : Tʜɪs ᴘᴇʀᴍᴀɴᴇɴᴛ Lɪɴᴋ, Nᴏᴛ Exᴘɪʀᴇᴅ</b>\n
-<i>© @AvishkarPatil </i>"""
+            msg_text ="""
+    <b><u> لینک دانلود آمادست</u></b>\n
+    <b>📂 نام فایل :</b> <i>{}</i>\n
+    <b>📦 اندازه فایل :</b> <i>{}</i>\n
+    <b>📥 لینک دانلود :</b> <i>{}</i>\n
+    <b>🚸 نکته : لینک پس از 24 ساعت منقضی می شود</b>\n
+    <i>🍃 کانال ما :</i> <b>@alphadl</b>
+    """
+            if await db.check_status(m.from_user.id) == True:
+                if await db.ckeck_dailyusage(m.from_user.id, size_gb(file_size)) == True:
+                    await db.set_data(log_msg.message_id, int(datetime.timestamp(datetime.now() + timedelta(hours=24))))
+                    await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Dᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True, parse_mode="Markdown", quote=True)
+                    await m.reply_text(
+                        text=msg_text.format(file_name, file_size, stream_link),
+                        parse_mode="HTML", 
+                        disable_web_page_preview=True,
+                        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("دانلود 📥", url=stream_link)]]),
+                        quote=True
+                    )
+                else:
+                    await m.reply_text(
+                        text='''**حجم استفاده روزانه شما کافی نیست**\n
+حجم باقی مانده: {}'''.format(db.info(m.from_user.id)['dailyUsage']),
+                        parse_mode="Markdown",
+                        disable_web_page_preview=True
 
-        await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Dᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True, parse_mode="Markdown", quote=True)
-        await m.reply_text(
-            text=msg_text.format(file_name, file_size, stream_link),
-            parse_mode="HTML", 
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ ɴᴏᴡ 📥", url=stream_link)]]),
-            quote=True
-        )
-    except FloodWait as e:
-        print(f"Sleeping for {str(e.x)}s")
-        await asyncio.sleep(e.x)
-        await c.send_message(chat_id=Var.BIN_CHANNEL, text=f"Gᴏᴛ FʟᴏᴏᴅWᴀɪᴛ ᴏғ {str(e.x)}s from [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n\n**𝚄𝚜𝚎𝚛 𝙸𝙳 :** `{str(m.from_user.id)}`", disable_web_page_preview=True, parse_mode="Markdown")
+                    )
+            else:
+                await m.reply_text(
+                    text='''شما اشتراک تدارید برای خرید اشتراک به ایدی زیر پیام دهید.\n
+<a href='https://t.me/alphadlsup'> AlphaDL Suport</a>''',
+                        parse_mode="HTML",
+                        disable_web_page_preview=True
+                )
+        except FloodWait as e:
+            print(f"Sleeping for {str(e.x)}s")
+            await asyncio.sleep(e.x)
+            await c.send_message(chat_id=Var.BIN_CHANNEL, text=f"Gᴏᴛ FʟᴏᴏᴅWᴀɪᴛ ᴏғ {str(e.x)}s from [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n\n**𝚄𝚜𝚎𝚛 𝙸𝙳 :** `{str(m.from_user.id)}`", disable_web_page_preview=True, parse_mode="Markdown")
 
 
 @StreamBot.on_message(filters.channel & (filters.document | filters.video) & ~filters.edited, group=-1)
